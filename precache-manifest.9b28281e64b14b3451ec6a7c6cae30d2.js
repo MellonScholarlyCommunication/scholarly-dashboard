a74f7dc46bdf475a3f5c46a2ad9c9bcccfc87ac5
dexagod <Ruben.Dedecker@Ugent.be>
2021-03-29 09:43:40 +0200

    Updates

---
+++ precache-manifest.9b28281e64b14b3451ec6a7c6cae30d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb2fee201f83085ef6d4be393b15185",
+    "url": "/scholarly-browser/index.html"
+  },
+  {
+    "revision": "72e607a8187f80ba7e6b",
+    "url": "/scholarly-browser/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "b750d250401c5dfd344c",
+    "url": "/scholarly-browser/static/css/main.cb09ae0d.chunk.css"
+  },
+  {
+    "revision": "72e607a8187f80ba7e6b",
+    "url": "/scholarly-browser/static/js/2.4016c8ad.chunk.js"
+  },
+  {
+    "revision": "7fdb782b787e98fdcbbe9ae5774d30cc",
+    "url": "/scholarly-browser/static/js/2.4016c8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b750d250401c5dfd344c",
+    "url": "/scholarly-browser/static/js/main.b46208d9.chunk.js"
+  },
+  {
+    "revision": "a5fdccee0985f40be608",
+    "url": "/scholarly-browser/static/js/runtime-main.6524ae0b.js"
+  }
+]);
